24521d01cd02616fd2ea23b6463f4d46855cec9b
CumAss <habibmazidov5@gmail.com>
2022-12-01 17:50:42 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+//  1.Ogaruvchanlar turlari let const (const ozgartrib bolgaydi)
+
+//  2.Number Boolean Undefiled Null String 
+
+//  3.increment oshuvchi Decrement kamayuvchi
+
+//  4.String + Number = String
+
+//  5.
+
+//  6.if else berilgan kiymatni false true likini aniklaydi
+
+//  7.else {if()}
+
+//  8.massiv ozidagi elementlarni qotor boyicha tartiblaydi. (arr[length-1])
+
+//  9.object ozida malumotni nomi va qiymati bilan saqlaydi 
+
+//  10.dasturlash tillarni ozida bolgan funksiyalar bu methodlar deyiladi 
+
+
+function sum1 (a,b){
+    console.log(a+b);
+}
+sum1(3,1);
+
+function sum2 (yosh){
+    let yil = 2022
+    console.log( yil-yosh);
+}
